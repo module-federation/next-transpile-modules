e5f77f3c0fcecda28330fe3e78dfe495ed231bf7
Zack Jackson <zackary.l.jackson@gmail.com>
2020-11-17 20:41:41 -0800

    remove caching config

--- src/next-transpile-modules.js
+++ src/next-transpile-modules.js
@@ -336,10 +336,6 @@ const withTmInitializer = (modules = [], options = {}) => {
           config.snapshot = Object.assign(snapshot, {
             managedPaths: cacheablePackages,
           });
-
-          config.cache = {
-            type: 'memory',
-          };
         }
         // Overload the Webpack config if it was already overloaded
         if (typeof nextConfig.webpack === "function") {
